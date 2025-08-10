bc0b115e088ca299ad08f8f5952191452f73210e
Info-God <karanvel.2005@gmail.com>
2025-08-10 21:58:12 +0530

    preparation start day 1

---
+++ src/OOPS/Oops1.java
@@ -0,0 +1,44 @@
+package OOPS;
+
+public class Oops1 {
+
+    public static void main(String[] args) {
+//        Student[] it; // declaring the array of students object that pointing to the Student class
+//        it = new Student[5];
+//        System.out.println(it[1]);
+
+            Student karan = new Student();
+            Student leena = karan;
+
+            System.out.println(leena.age);
+            System.out.println(leena.department);
+            karan.Echo();
+
+
+        }
+
+    // This is class that create a rule we use this to create different object
+
+
+
+
+}
+
+class Student {
+    int age;
+    String department;
+
+
+    void Echo(){
+        System.out.println("hello my age is "+ age);
+    }
+
+    Student(){
+        this (12, "cse");
+    }
+
+    Student(int newage , String dept){
+        age = newage;
+        department = dept;
+    }
+}
