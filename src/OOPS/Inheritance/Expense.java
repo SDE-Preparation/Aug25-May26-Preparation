0e68af1ae7e7985f9c3a2e3d2d8b2d22017db288
Info-God <karanvel.2005@gmail.com>
2025-08-13 22:25:13 +0530

    Inheritance

---
+++ src/OOPS/Inheritance/Expense.java
@@ -0,0 +1,14 @@
+package OOPS.Inheritance;
+
+public class Expense {
+    static int totalWealth = 50000;
+
+    Expense(int spent){
+        Expense.totalWealth -= spent;
+    }
+
+    public static void checkWealth(){
+        System.out.println("Total amount is " + Expense.totalWealth);
+    }
+
+}
---
+++ src/OOPS/Inheritance/Son.java
@@ -0,0 +1,11 @@
+package OOPS.Inheritance;
+
+
+
+public class Son {
+    public static void main(String[] args) {
+        Expense karan = new Expense(5000);
+        Expense agi = new Expense(3000);
+        Expense.checkWealth();
+    }
+}
