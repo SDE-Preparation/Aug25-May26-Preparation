a3f90428e6a14687a35e374b64f492132cdf2001
Info-God <karanvel.2005@gmail.com>
2025-08-24 23:29:46 +0530

    Daily update - 2025-08-24

---
+++ src/Dsa/Arrays/Subset.java
@@ -0,0 +1,90 @@
+package Dsa.Arrays;
+
+import java.util.HashMap;
+
+public class Subset {
+
+    public static Boolean isSubset(int[] main, int[] sub){
+        if(sub.length > main.length){
+            return false;
+        }
+        Boolean isThere = false;
+
+        for(int eachSub : sub){
+            isThere = false;
+            for(int eachMain : main){
+                if(eachMain == eachSub){
+                    isThere = true;
+
+                    break;
+                }
+            }
+
+            if(!isThere){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static Boolean isSubset2(int[] main, int[] sub){
+        HashMap<Integer,Integer> mainCount = new HashMap<>();
+        HashMap<Integer,Integer> subCount = new HashMap<>();
+        int count = 0;
+
+
+        for(int i = 0; i< main.length; i++){
+            count = 0;
+            for(int j = 0; j< main.length; j++){
+                if(main[i] == main[j]){
+                    count++;
+                }
+            }
+
+            mainCount.put(main[i],count);
+        }
+
+        for(int i = 0; i< sub.length; i++){
+            count = 0;
+            for(int j = 0; j< sub.length; j++){
+                if(sub[i] == sub[j]){
+                    count++;
+                }
+            }
+
+            subCount.put(sub[i],count);
+        }
+
+//        for(int j = 0;j < mainCount.size();j++){
+//            for(int z = 0; z < subCount.size();z++){
+//                if(mainCount.get())
+//            }
+//        }
+        Boolean keyExist = false;
+        for(Integer subKey : subCount.keySet()){
+            keyExist = false;
+            for (Integer key : mainCount.keySet()) {
+                if(subKey.equals(key)){
+                    keyExist = true;
+                    if(mainCount.get(key) < subCount.get(subKey)){
+                        return false;
+                    }
+                }
+            }
+
+            if(!keyExist){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+    public static void main(String[] args) {
+        int[] main = { 1, 2, 2, 3, 3, 3};
+        int[] sub = { 3, 5, 3};
+
+        System.out.println(isSubset2(main,sub));
+    }
+}
