2c8543aaaf780e1ddae47599617c300ed4316e84
Info-God <karanvel.2005@gmail.com>
2025-08-26 22:58:49 +0530

    Daily update - 2025-08-26

--- src/Dsa/Arrays/MissingArrays.java
+++ src/Dsa/Arrays/MissingArrays.java
@@ -1,22 +1,30 @@
 package Dsa.Arrays;
-
+import java.util.*;
 public class MissingArrays {
 
     static int missingValue(int[] arr){
+        Arrays.sort(arr);
+        if(arr.length == 1){
+            return arr[0] + 1;
+        }
+        int result = 0;
         for(int i = 1 ; i < arr.length; i++){
 
                 if(arr[i-1] + 1 != arr[i]){
-                    return arr[i-1] + 1;
+                    result = arr[i-1] + 1;
+                }
+                if(arr.length - 1 == i){
+                    result = arr[i] + 1;
                 }
         }
 
 
-        return -1;
+        return result;
     }
 
     public static void main(String[] args) {
         int[] arr = {
-                1, 2, 3,4, 5, 7
+                2,6,5,1,3
         };
 
         System.out.println(missingValue(arr));
