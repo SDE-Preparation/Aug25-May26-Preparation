927d31e9dddf717c69b847a915dca87bc3f9342e
Info-God <karanvel.2005@gmail.com>
2025-08-27 22:00:10 +0530

    Daily update - 2025-08-27

---
+++ src/LibraryManagement/Main.java
@@ -0,0 +1,33 @@
+package LibraryManagement;
+
+import java.util.*;
+
+//class Book {
+//    // Here i declare what are the data that contain by book
+//    String title;
+//    String author;
+//    String genre;
+//    boolean isAvailable;
+//
+//    // Initialize data by calling constructor/creating an object
+//    Book(String title, String author, String genre, boolean isAvailable){
+//        this.title = title;
+//        this.author = author;
+//        this.genre = genre;
+//        this.isAvailable = isAvailable;
+//    }
+//}
+//
+//class Member {
+//    String name;
+//    List<String> barrowBooks = new ArrayList<>();
+//
+//    public boolean barrowBook(Book book){
+//
+//    }
+//}
+
+
+public class Main {
+
+}
