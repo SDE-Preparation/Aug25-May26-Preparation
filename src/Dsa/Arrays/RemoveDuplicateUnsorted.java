e19b289900915e24fa573abc6581fc0ebabd0caa
Info-God <karanvel.2005@gmail.com>
2025-08-30 22:00:07 +0530

    Daily update - 2025-08-30

--- src/Dsa/Arrays/RemoveDuplicateUnsorted.java
+++ src/Dsa/Arrays/RemoveDuplicateUnsorted.java
@@ -16,8 +16,23 @@ public class RemoveDuplicateUnsorted {
         return arr;
     }
 
+
+    static ArrayList<Integer> removeDup(int[] arr){
+
+        ArrayList<Integer> storeUnique = new ArrayList<>();
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(!storeUnique.contains(arr[i])){
+                storeUnique.add(arr[i]);
+            }
+        }
+
+        return storeUnique;
+    }
+
     public static void main(String[] args) {
         int[] arr =  { 1, 4 ,4 , 6, 3, 4};
-        System.out.println(Arrays.toString(removeDuplicate(arr)));
+        System.out.println(removeDup(arr));
     }
 }
