7c1b1e4cfcc73082156e1fb02866204a41a86409
Info-God <karanvel.2005@gmail.com>
2025-09-22 22:00:10 +0530

    Daily update - 2025-09-22

--- src/Dsa/Arrays/Searching/LowerBound.java
+++ src/Dsa/Arrays/Searching/LowerBound.java
@@ -1,6 +1,30 @@
 package Dsa.Arrays.Searching;
 
 public class LowerBound {
-    
 
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 5, 7, 8, 99};
+
+        int start = 0;
+
+        int target =  100;
+
+        int end = arr.length - 1;
+        int lb = arr.length;
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] >= target){
+                lb = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(lb);
+    }
 }
