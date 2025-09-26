96daa1cf611eb2a15819d20ec5ee165ecc7a3460
Info-God <karanvel.2005@gmail.com>
2025-09-26 22:00:07 +0530

    Daily update - 2025-09-26

---
+++ src/Dsa/Arrays/Searching/findPeekInMountainArray.java
@@ -0,0 +1,31 @@
+package Dsa.Arrays.Searching;
+
+public class findPeekInMountainArray {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 8,9,9, 4, 2};
+
+
+        System.out.println(findPeak(arr));
+    }
+
+    static int findPeak(int[] arr){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start)/ 2;
+
+            if(arr[mid] > arr[mid+1]) {
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return arr[start];
+    }
+}
