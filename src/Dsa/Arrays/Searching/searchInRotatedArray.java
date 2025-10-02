31ba6748d8a7add81f4685b356c0e7a8cd3a29e0
Info-God <karanvel.2005@gmail.com>
2025-10-02 22:00:10 +0530

    Daily update - 2025-10-02

--- src/Dsa/Arrays/Searching/searchInRotatedArray.java
+++ src/Dsa/Arrays/Searching/searchInRotatedArray.java
@@ -5,28 +5,40 @@ public class searchInRotatedArray {
     public static void main(String[] args) {
         int[] arr = { 5, 6 , 7, 1, 2, 3};
 
-        System.out.println(search(arr,5));
+        System.out.println(search(arr,3));
     }
 
     static int search(int[] arr, int target){
-
-    }
-
-    static int pivotFind(int[] arr){
-
         int start = 0;
+
         int end = arr.length - 1;
 
-        while(start < end){
 
-            int mid = start + (end - start) / 2;
+        while(start <= end ){
 
-            if(arr[mid] > arr[mid + 1]){
+            int mid = start + ( end - start ) / 2;
+
+            if(arr[mid] == target){
                 return mid;
-            }else if(arr[mid] < arr[mid + 1]){
-                start = mid
+            }else if(arr[start] <= arr[mid] ){
+
+                if(target >= arr[start]  && target < arr[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }else{
+                if(target > arr[mid] && target <= arr[end]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
             }
         }
+
+        return -1;
     }
+
+
 
 }
