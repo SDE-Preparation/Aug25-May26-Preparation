0cf636c42e1d9baea1f9c2d20f09bb1a39571ec2
Info-God <karanvel.2005@gmail.com>
2025-10-22 22:00:11 +0530

    Daily update - 2025-10-22

---
+++ src/Dsa/Arrays/Subarrays/LongestSubArray.java
@@ -0,0 +1,36 @@
+package Dsa.Arrays.Subarrays;
+
+import java.util.*;
+
+public class LongestSubArray {
+    public static int longestSubarray(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+
+            // If (sum - k) was seen before, subarray between (oldIndex+1 → i) = k
+            if (map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            // Store first occurrence only (for longest)
+            map.putIfAbsent(sum, i);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 1, 1};
+        int k = 3;
+        System.out.println("Longest subarray length = " + longestSubarray(arr, k));
+    }
+}
--- src/Dsa/Arrays/Subarrays/PrefixSumWithHashMap.java
+++ src/Dsa/Arrays/Subarrays/PrefixSumWithHashMap.java
@@ -14,14 +14,17 @@ public class PrefixSumWithHashMap {
     public static int subarraySum(int[] nums, int k) {
         int sum = 0;
         int result = 0;
+        int max = 0;
         Map<Integer, Integer> prefixMap = new HashMap<>();
         prefixMap.put(0, 1); // base case
 
-        for (int num : nums) {
-            sum += num;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
 
             // if prefix[r] - k exists, we found valid subarray(s)
             if (prefixMap.containsKey(sum - k)) {
+
+
                 result += prefixMap.get(sum - k);
             }
 
@@ -33,27 +36,29 @@ public class PrefixSumWithHashMap {
     }
 
 
-    public static int subArrayMaxSum(int[] nums, int k){
-
-        int count = 0;
-        int sum = 0;
-        int max = 0;
-
-        Map<Integer,Integer> prefixSum = new HashMap<>();
 
 
-        prefixSum.put(0,1);
-
-        for(int num = 0; num < nums.length; num++){
-
-            sum += nums[num];
-
-            if(prefixSum.containsKey(sum - k)){
-                if(max == 0){
-                    max = 
-                }
-            }
-        }
-    }
+//    public static int subArrayMaxSum(int[] nums, int k){
+//
+//        int count = 0;
+//        int sum = 0;
+//        int max = 0;
+//
+//        Map<Integer,Integer> prefixSum = new HashMap<>();
+//
+//
+//        prefixSum.put(0,1);
+//
+//        for(int num = 0; num < nums.length; num++){
+//
+//            sum += nums[num];
+//
+//            if(prefixSum.containsKey(sum - k)){
+//                if(max == 0){
+//                    max =
+//                }
+//            }
+//        }
+//    }
 
 }
