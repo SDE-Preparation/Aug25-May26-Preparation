60c9a8f2e735f36a43777971367b46b834cd3dc9
Info-God <karanvel.2005@gmail.com>
2025-12-28 22:00:07 +0530

    Daily update - 2025-12-28

---
+++ src/Dsa/Arrays/Restart/ReverseAnArray.java
@@ -0,0 +1,28 @@
+package Dsa.Arrays.Restart;
+
+import java.util.Arrays;
+
+public class ReverseAnArray {
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 5, 6 ,9};
+
+        int start = 0;
+
+        int end = arr.length - 1;
+
+        while(start < end){
+
+            int temp = arr[start];
+
+            arr[start] = arr[end];
+
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
